7bb1e3a8e0e5fe28a7b65fad0bf7c1915ac22a85
Manuel Luckey <info@computer-spezialisten.de>
2024-09-10 14:35:29 +0200

    Update OSD-Cloud-Skript.ps1

--- OSD-Cloud-Skript.ps1
+++ OSD-Cloud-Skript.ps1
@@ -1,9 +1,10 @@
 Write-Host -ForegroundColor Green "Starting OSDCloud ZTI"
 Start-Sleep -Seconds 5
-Write-Host "Drücken Sie eine beliebige Taste, um fortzufahren..."
+Write-Host "Druecken Sie eine beliebige Taste, um fortzufahren..."
+Write-Host "Beachten Sie das die gesammte Festplatte Formatiert wird, so mit alle Daten auf dem PC gelöscht. Sollten dies nicht umgesetzt werden, PC Herunterfahren und USB-Stick entfernen."
 [void][System.Console]::ReadKey($true)  # Warten auf eine beliebige Taste
 
-Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 23H2 -OSEdition Pro -OSLanguage de-de -OSLicense Retail -ZTI
+Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 22H2 -OSEdition Pro -OSLanguage de-de -OSLicense Retail -ZTI
 
 #Restart from WinPE
 
